9506000628a91bbf0c145ee86674a8f732454dba
ArkarD1Asia <arkarmin.dev007@gmail.com>
2025-04-08 15:01:30 +0700

    Add files via upload

---
+++ LogMonitor/LogMonitor/LogMonitor/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿using System.Windows.Forms.VisualStyles;
+
+namespace LogMonitor
+{
+    partial class FileViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Generate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.txt_Terminal = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 100);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Terminal No";
+            // 
+            // btn_Generate
+            // 
+            this.btn_Generate.Location = new System.Drawing.Point(140, 184);
+            this.btn_Generate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Generate.Name = "btn_Generate";
+            this.btn_Generate.Size = new System.Drawing.Size(168, 53);
+            this.btn_Generate.TabIndex = 2;
+            this.btn_Generate.Text = "Generate";
+            this.btn_Generate.UseVisualStyleBackColor = true;
+            this.btn_Generate.Click += new System.EventHandler(this.btn_Generate_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblStatus);
+            this.groupBox1.Controls.Add(this.txt_Terminal);
+            this.groupBox1.Controls.Add(this.btn_Close);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btn_Generate);
+            this.groupBox1.Location = new System.Drawing.Point(32, 36);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(768, 306);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "File Viewer Application";
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(336, 184);
+            this.btn_Close.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(134, 53);
+            this.btn_Close.TabIndex = 3;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // txt_Terminal
+            // 
+            this.txt_Terminal.AutoSize = true;
+            this.txt_Terminal.Location = new System.Drawing.Point(135, 100);
+            this.txt_Terminal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txt_Terminal.Name = "txt_Terminal";
+            this.txt_Terminal.Size = new System.Drawing.Size(0, 25);
+            this.txt_Terminal.TabIndex = 4;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.lblStatus.Location = new System.Drawing.Point(137, 140);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 25);
+            this.lblStatus.TabIndex = 5;
+            // 
+            // FileViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 374);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FileViewer";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Generate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Label txt_Terminal;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ LogMonitor/LogMonitor/LogMonitor/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace LogMonitor
+{
+    public partial class FileViewer : Form
+    {
+        private readonly string _logFolderPath = @"F:/GrgBanking/eCAT/Log/";
+        private readonly string _imgFolderPath = @"F:/CameraImage/Image/";
+        private readonly string _outputFolderPath = @"F:/FileViewer/Output/";
+        private string getTerminalId = "";
+        public FileViewer()
+        {
+            InitializeComponent();
+            LoadATMNumber();
+        }
+
+        private void btn_Generate_Click(object sender, EventArgs e)
+        {
+            btn_Generate.Enabled = false;
+            if (string.IsNullOrEmpty(getTerminalId))
+            {
+                btn_Generate.Enabled = true;
+                MessageBox.Show("Please enter the terminal No.","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                string outputFileName = $"{getTerminalId.Trim()}_{DateTime.Now:yyyyMMdd}_FileViewer.txt";
+                if (!Directory.Exists(_outputFolderPath))
+                {
+                    Directory.CreateDirectory(_outputFolderPath);
+                }
+                string outputFilePath = Path.Combine(_outputFolderPath, outputFileName);
+
+                DateTime oneYearCutoff = DateTime.Today.AddYears(-1);
+                DateTime threeMonthsCutoff = DateTime.Today.AddMonths(-3);
+
+                using(StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    var logFiles = Directory.GetFiles(_logFolderPath, "*.*", SearchOption.AllDirectories)
+                                   .Where(f => !File.GetAttributes(f).HasFlag(FileAttributes.Directory))
+                                   .Where(f => f.EndsWith(".txt", StringComparison.OrdinalIgnoreCase) ||
+                                               f.EndsWith(".zip", StringComparison.OrdinalIgnoreCase));
+
+                    foreach(string file in logFiles)
+                    {                        
+                        DateTime creationDate = File.GetCreationTime(file);
+                        DateTime lastWriteDate = File.GetLastWriteTime(file);
+
+                        bool olderOneYear = creationDate > oneYearCutoff || lastWriteDate > oneYearCutoff;
+
+                        if (olderOneYear)
+                        {
+                            writer.WriteLine(file);
+                            writer.WriteLine();
+                        }
+                    }
+
+                    var imgFiles = Directory.GetDirectories(_imgFolderPath)
+                                    .Where(folder =>
+                                    {
+                                        string folderName = Path.GetFileName(folder);
+                                        return folderName.Length == 8 && folderName.All(char.IsDigit);
+                                    });
+
+                    foreach (string folder in imgFiles) 
+                    {
+                        string folderName = Path.GetFileName(folder);
+                        if (DateTime.TryParseExact(folderName, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out DateTime folderDate))
+                        {
+                            bool olderThreeMonth = folderDate > threeMonthsCutoff;
+
+                            if (olderThreeMonth)
+                            {
+                                var zipFiles = Directory.GetFiles(folder, "*.zip");
+                                foreach (string zipFile in zipFiles)
+                                {
+                                    writer.WriteLine($"{zipFile.Replace("\\","/")}");
+                                    writer.WriteLine();
+                                }                                   
+                            }
+                        }
+
+                        
+                    }
+                    lblStatus.Text= "Report generated successfully";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = ex.Message;
+            }
+
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        #region "GetTerminalInfo"
+        private void LoadATMNumber()
+        {
+            try
+            {
+                string filePath = @"D:\GrgBanking\eCAT\Config\TerminalConfig.xml";
+
+                XDocument xmlDoc = XDocument.Load(filePath);
+
+                string atmNumber = xmlDoc.Root.Element("Terminal")?.Attribute("ATMNumber")?.Value;
+                txt_Terminal.Text = atmNumber ?? "ATMNumber not found";             
+                 getTerminalId=atmNumber;
+    }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error reading XML: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        #endregion
+    }
+}
